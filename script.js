1f66f9383b058b92338409d894c3980ae7ec36a4
a5tup3d_guy <juanpablohdz2202@gmail.com>
2026-09-11 20:34:32 -0600

    changed button function

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const boton = document.getElementById('btnSoundtrack');
 
+const etiquetaAudio = document.createElement('audio');
+etiquetaAudio.setAttribute('src', 'thousand-march.mp3');
+document.body.appendChild(etiquetaAudio); 
+
 boton.addEventListener('click', () => {
-  const etiquetaAudio = document.createElement('audio');
-  etiquetaAudio.setAttribute('src', 'thousand-march.mp3');
-  
-  document.body.appendChild(etiquetaAudio); 
+  etiquetaAudio.currentTime = 0; 
   
   etiquetaAudio.play().catch(error => {
     console.error("Playback failed:", error);
